3c8205856ffc6bf498295cdb70d092ac748b82df
Dylan Pan <Dylan Pan>
2016-11-28 19:04:15 -0500

    Just testing github

--- screenclass.py
+++ screenclass.py
@@ -10,16 +10,15 @@ class Screen:
 		background = pygame.image.load(self.imagereference).convert()
 		#i don't know if you can refer to self.imagereference like that, but this is the logic behind it. also why couldn't you?
 		screen.blit(background, [0,0])
-		
+
 		done = False
 		clock = pygame.time.Clock()
 
 
-#this is the bare bones of the screen class. i'm not done at all. the whole idea is that it is more general so that each screen (introduction, start, game, shop) use the same class, and we are not "hardcoding" as much as we currently are. see concerns listed in group chat- Crystal 
+#this is the bare bones of the screen class. i'm not done at all. the whole idea is that it is more general so that each screen (introduction, start, game, shop) use the same class, and we are not "hardcoding" as much as we currently are. see concerns listed in group chat- Crystal
 
 #http://www.balloonbuilding.com/index.php?chapter=introduction_to_classes&lang=en#section_12
 #that's one of the internet sources that I found useful which explained classes. Usually they should be more general so that more instances of that class can be used -- aka code is more efficient
-		
 
-		
 
+# example test for git
