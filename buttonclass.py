1e40e1df627ac9786985467bed38912f588954ff
crystallow97 <crystallow93@gmail.com>
2016-11-30 18:09:42 -0500

    Update buttonclass.py

--- buttonclass.py
+++ buttonclass.py
@@ -2,9 +2,13 @@ import pygame
 import os
 import sys
 
+
+#COLORS
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+
 class Button:
 	def __init__(self, screen, color, x, y, width, height):
 		pygame.display.init()
@@ -15,23 +19,8 @@ class Button:
 		self.width = width
 		self.color = color
 	
-		#pygame.draw.rect(screen,color, ((x pos, y pos), (width, height)), thickness)
-		
+		#pygame.draw.rect(screen,color, ((x pos, y pos), (width, height)), thickness) these are the parameters
 		self.rect = pygame.draw.rect(screen, self.color, ((self.x,self.y), (self.width, self.height)), 0)
-
-		
-
-		
-
-
-
-#screen = pygame.display.set_mode([800,600])
-#pygame.display.set_caption("Arcade City")
-	
-		
-		#background = pygame.image.load("imagetest.jpg").convert()
-		#screen.blit(background,[0,0]) 
-
 
 
 
@@ -44,13 +33,12 @@ clock = pygame.time.Clock()
 clock.tick(60)
 pygame.display.flip()
 
-
-done = False
 #while loop
 
 #the collision needs to be a conditional in the while loop. 
-
 #THE BUTTON CLASS ALLOWS YOU TO DO THIS FOR EVERY BUTTON.
+
+done = False
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
