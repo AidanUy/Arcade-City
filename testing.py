3a1bc0318082d56f0f096e8f51c7adfd2476f0f6
root <root@debian>
2016-11-29 19:59:06 -0500

    test file

--- testing.py
+++ testing.py
@@ -5,16 +5,9 @@ import screenclass
 
 
 def main():
+
 	from screenclass import Screen
 	a_screen = Screen()
-	a_screen.background1
-	#print(home_screen.imagereference)
-	#background = pygame.image.load("imagetest.jpg").convert()
-	#screen.blit(background, [0,0])
-	#home_screen.imagereference = pygame.image.load("imagetest.jpg").convert()
+	a_screen.background2
 	
-	#trying to see if blit would work on this test file, BUT IT WILL NOT. not self.screen.blit on the main file will not work
-	#either, because screen is defined in the class file.
-	
-	#EVERYTHING IN THE HASHTAG ARE FAILED ATTEMPTS!!!!!!!!!!!
 main()
