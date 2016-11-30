b3486dd05b37cf306fbe486d0ffbb2cdac6a739d
root <root@debian>
2016-11-29 19:57:02 -0500

    edited

--- screenclass.py
+++ screenclass.py
@@ -4,17 +4,26 @@ import sys
 class Screen():
 	def __init__(self):
 		size = [800, 600]
+
+		#pick from one of these WORKS
 		screen = pygame.display.set_mode(size)
 		pygame.display.set_caption("Arcade City")
 
 		self.background1 = pygame.image.load("imagetest.jpg").convert()
-		#self.background2 = pygame.image.load("
+		self.background2 = pygame.image.load("imagetest2.jpg").convert()
 
 		#background = pygame.image.load(self.imagereference).convert()
 		#referring to self.imageref inside background does not work because self.imagereference is currently blank
-		screen.blit(background1,[0,0])
+		
+		if self.background1:
+			screen.blit(self.background1,[0,0])
+		else:
+			screen.blit(self.background2, [0,0])
+		
 		clock = pygame.time.Clock()
 		clock.tick(60)
+		
+		pygame.display.flip()
 
 		done = False
 		#while loop-----
@@ -34,3 +43,4 @@ class Screen():
 		
 
 		
+
