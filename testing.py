98e86a35ee6feafc202edebddce040a2ad312e64
Dylan Pan <dpan117@gmail.com>
2016-11-29 23:50:31 -0500

    Added sprite functionality

--- testing.py
+++ testing.py
@@ -1,13 +1,53 @@
-import sys
 import pygame
-import screenclass 
+pygame.init()
 
+class gameSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load("testsprite.png").convert()
+        self.x = 0
+        self.y = 0
 
+    def update(self):
+        key = pygame.key.get_pressed()
+        dist = 5
+        if key[pygame.K_DOWN]:
+            self.y += dist
+        elif key[pygame.K_UP]:
+            self.y -= dist
+        if key[pygame.K_RIGHT]:
+            self.x += dist
+        elif key[pygame.K_LEFT]:
+            self.x -= dist
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
 def main():
+    size = [800, 600]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Arcade City")
 
-	from screenclass import Screen
-	a_screen = Screen()
-	a_screen.background2
-	#hello world
+    clock = pygame.time.Clock()
+
+    done = False
+
+    white = (255, 255, 255)
+
+    character = gameSprite()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                done = True
+
+        character.update()
+
+        screen.fill(white)
+        character.draw(screen)
+
+        pygame.display.update()
+
+        clock.tick(60)
+
 main()
