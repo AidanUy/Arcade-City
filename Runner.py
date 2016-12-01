c31c3a6f6e521f5b58041d6b5b1a20fe8f1bc9c2
Aidan Uy <aidanuy1@gmail.com>
2016-12-01 16:38:12 -0500

    Main runner class

--- Runner.py
+++ Runner.py
@@ -9,8 +9,7 @@ class Game:
     def __init__(self):
         self.tickets = 0
 
-    def startScreen(self, screen):
-        
+    def startScreen(self, screen, win):
         titleText = pygame.font.SysFont('Showcard Gothic', 60)
         subText = pygame.font.SysFont('Showcard Gothic', 20)
 
@@ -34,13 +33,63 @@ class Game:
         start = buttonText.render("Start!", True, WHITE)
         screen.blit(start, [365, 425])
 
-        #if 494 > mouse[0] > 343 and 461 > mouse[1] > 411:
-        #    if event.type == pygame.MOUSEBUTTONDOWN:
-        #        from instructions.py import instructionScreen
-
+        for event in pygame.event.get():
+             if 494 > mouse[0] > 343 and 461 > mouse[1] > 411:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    win.instructionScreen(screen, win)
+                    pygame.display.update()
+                    return
+                    
         pygame.display.update()
 
+    def instructionScreen(self, screen, win):
+        
+        background = pygame.image.load("background.png").convert()
+        screen.blit(background, [0, 0])
+        caption = pygame.image.load("caption.png").convert()
+        oak = pygame.image.load("oak.png").convert()
+        oak.set_colorkey(BLACK)
+        screen.blit(oak, [570, 130])
+        titleText = pygame.font.SysFont('Showcard Gothic', 60)
+        subText = pygame.font.SysFont('Showcard Gothic', 25)
 
+        text = titleText.render("Instructions", True, WHITE)
+        captionText = subText.render("Hey! Welcome to Arcade City! Start by walking up", True, BLACK)
+        captionText2 = subText.render("to and playing Higher or Lower and racking up", True, BLACK)
+        captionText3 = subText.render("tickets. Then, when you get enough tickets,", True, BLACK)
+        captionText4 = subText.render("different games will be unlocked. Have fun!", True, BLACK)
+
+        screen.blit(text, [200, 80])
+
+        mouse = pygame.mouse.get_pos()
+
+        if 480 > mouse[0] > 325 and 550 > mouse[1] > 500:
+            pygame.draw.rect(screen, RED, (325, 500, 150, 50))
+        else:
+            pygame.draw.rect(screen, GREEN, (325, 500, 150, 50))
+
+        buttonText = pygame.font.SysFont('Showcard Gothic', 30)
+
+        screen.blit(caption, [3, 300])
+        pygame.draw.rect(screen, WHITE, (45, 320, 670, 110))
+
+        screen.blit(captionText, [45, 325])
+        screen.blit(captionText2, [45, 350])
+        screen.blit(captionText3, [45, 375])
+        screen.blit(captionText4, [45, 400])
+
+        play = buttonText.render("Play!", True, WHITE)
+        screen.blit(play, [357, 515])
+
+        #if 480 > mouse[0] > 325 and 550 > mouse[1] > 500:
+        #    if event.type == pygame.MOUSEBUTTONDOWN:
+        #        from gamescreen.py import gamescreen
+
+        pygame.display.update()
+        
+    def clearScreen(self, screen):
+        screen.fill(WHITE)
+        
 def main():
     pygame.init()
     size = [800, 600]
@@ -55,7 +104,7 @@ def main():
                 pygame.quit()
                 done = True
                 
-        win.startScreen(screen)
+        win.startScreen(screen, win)
         clock.tick(60)
 if __name__ == "__main__":
     main()
