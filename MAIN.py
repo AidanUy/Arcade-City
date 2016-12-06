aa9980c83c59dd3fe5d01b0e978c81c015ad42c3
Crystal Low <crystallow93@gmail.com>
2016-12-06 15:42:41 -0500

    buttons now work, problem is is that clicking anywhere on screen in instructions brings back to start

--- MAIN.py
+++ MAIN.py
@@ -1,24 +1,34 @@
 import pygame, os, sys
+from buttonclass import button
 from startscreen import startScreen
 from instructions import instructionScreen
 pygame.init()
 
 def main():
+    
+    #instruction = instructionScreen()
+    startScreen()
     done = False
     while not done:
-        startScreen()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                done = True
-
-            mouse = pygame.mouse.get_pos()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if 500 > mouse[0] > 350 and 450 > mouse[1] > 400:
-                    return instructionScreen()
-                    if 480 > mouse[0] > 325 and 550 > mouse[1] > 500:
-                        print("hello")
-
+               pygame.quit()
+               done = True
+           
+            if event.type == pygame.MOUSEBUTTONDOWN: 
+               startScreen()
+               mouse1 = pygame.mouse.get_pos()
+               #print(mouse1)
+               if 500 > mouse1[0] > 350 and 450 > mouse1[1] > 400:
+                   print("this button works")
+                   instructionScreen()
+                  # pygame.display.update() 
+           
+                   
+               if 475 > mouse1[0] > 325 and 550 > mouse1[1] > 500: 
+                  print(mouse1)
+                  print("aaa")
+                  
         pygame.display.update()
-
 main()
