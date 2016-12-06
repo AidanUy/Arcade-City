eaa1f1302e78a5ad61c6638c0a705a81494673d3
Dylan Pan <dpan117@gmail.com>
2016-12-05 23:35:43 -0500

    Minor tweaks

--- MAIN.py
+++ MAIN.py
@@ -1,18 +1,17 @@
 import pygame, os, sys
 from buttonclass import button
 from startscreen import startScreen
-
 pygame.init()
 
 def main():
     startScreen()
     done = False
     while not done:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                   pygame.quit()
-                   sys.exit()
-                   pygame.display.quit()
-                   done = True
-             
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               pygame.quit()
+               sys.exit()
+               pygame.display.quit()
+               done = True
+
 main()
--- higherOrLower.py
+++ higherOrLower.py
@@ -1,12 +1,24 @@
-
 import random
 import pygame
 import os
 import sys
+from buttonclass import button
 pygame.init()
 
 #correct = False
 #tickets = 0
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+red = (200, 0, 0)
+bright_red = (255, 0, 0)
+
+green = (0, 200, 0)
+bright_green = (0, 255, 0)
+
+blue = (0, 0, 200)
+bright_blue = (0, 0, 255)
 
 class higherOrLower:
     #def getRandNum():
@@ -16,29 +28,14 @@ class higherOrLower:
     def __init__(self): #and tickets
         size = [800, 600]
         screen = pygame.display.set_mode(size)
-        pygame.display.set_caption("Higher or Lower")
-
-        black = (0, 0, 0) 
-        white = (255, 255, 255)
-
-        red = (200, 0, 0)
-        bright_red = (255, 0, 0)
-
-        green = (0, 200, 0)
-        bright_green = (0, 255, 0)
-
-        blue = (0, 0, 200)
-        bright_blue = (0, 0, 255)
+        pygame.display.set_caption("Arcade City")
 
         clock = pygame.time.Clock()
-        
+
         currentNum = random.randrange(20)
 
         wins = 0
         tickets = 0
-
-##        titleText = pygame.font.SysFont('Showcard Gothic', 60)
-##        subText = pygame.font.SysFont('Showcard Gothic', 25)
 
         background = pygame.image.load("background.png").convert()
         screen.blit(background, [0,0])
@@ -47,24 +44,23 @@ class higherOrLower:
         oak.set_colorkey(black)
         screen.blit(oak, [570, 130])
 
+        done = False
         while (wins < 3 and wins > -1 or done == True):
             for event in pygame.event.get():
-                pygame.quit()
-                done = True
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    done = True
 
             titleText = pygame.font.SysFont('Showcard Gothic', 60)
             subText = pygame.font.SysFont('Showcard Gothic', 25)
 
-
-            done = False
-            
-##            text = titleText.render("Higher or Lower", True, white)
-##            p1 = subText.render("My number is " + str(currentNum), True, black)
-##            p2 = subText.render("Will my next number be higher or lower?", True, black)
-##            p3 = subText.render("Correct!",True, black)
-##            p4 = subText.render("Game over", True, black)
-##            pWin = subText.render("You win! Here is your ticket!", True, black)
-##            pLose = subText.render("You lose. Try again later!", True, black)
+            text = titleText.render("Higher or Lower", True, white)
+            p1 = subText.render("My number is " + str(currentNum), True, black)
+            p2 = subText.render("Will my next number be higher or lower?", True, black)
+            p3 = subText.render("Correct!",True, black)
+            p4 = subText.render("Game over", True, black)
+            pWin = subText.render("You win! Here is your ticket!", True, black)
+            pLose = subText.render("You lose. Try again later!", True, black)
 
             screen.blit(text, [200, 80])
 
@@ -78,11 +74,9 @@ class higherOrLower:
 
             print("My number is " + str(currentNum)) #p1
             newNum = random.randrange(20)
-            print("For debuging num is" + str(newNum)) 
+            print("For debuging num is" + str(newNum))
             hOrL = input("Will my next number be higher or lower? (\"h\" or \"l\")")#p2
 
-
-            
             if (not(newNum == currentNum)):
                 if (hOrL == "h" or hOrL == "H"):
                     if(newNum >= currentNum):
@@ -114,7 +108,6 @@ class higherOrLower:
 
             clock.tick(60)
 
-
 def main():
     print("Welcome to Higher or Lower!")
     print("I will tell you a number.")
@@ -122,4 +115,5 @@ def main():
     print("If you guess correctly 3 times in a row, you win a ticket!")
     tickets = higherOrLower()
     print("You now have " + str(tickets) + " tickets.")
+
 main()
