f6cb16355dad1d8a8f9096a4c41f0c46ed47345b
Crystal Low <crystallow93@gmail.com>
2016-12-06 16:33:12 -0500

    this is a screen meant to be used to test the main loop

---
+++ testingscreen.py
@@ -0,0 +1,57 @@
+import pygame
+import sys
+from buttonclass import button
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+red = (200, 0, 0)
+bright_red = (255, 0, 0)
+
+green = (0, 200, 0)
+bright_green = (0, 255, 0)
+
+blue = (0, 0, 200)
+bright_blue = (0, 0, 255)
+
+class testScreen:
+
+    def __init__(self):
+
+        size = [800, 600]
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption("Arcade City")
+
+    
+
+        clock = pygame.time.Clock()
+
+        background = pygame.image.load("background.png").convert()
+        screen.blit(background, [0, 0])
+
+        pygame.mixer.music.load("music.mp3")
+        pygame.mixer.music.play(loops = -1)
+        pygame.mixer.music.set_volume(0.5)
+
+       
+
+        titleText = pygame.font.SysFont('Showcard Gothic', 60)
+        subText = pygame.font.SysFont('Showcard Gothic', 20)
+ 
+        text = titleText.render("TESTING", True, white)
+        cs = subText.render("CS110 Final Project", True, white)
+        names = subText.render("Aidan Uy, Crystal Low, Danika Gaviola, Dylan Pan", True, white)
+
+        screen.blit(text, [220, 200])
+        screen.blit(cs, [310, 265])
+        screen.blit(names, [150, 290])
+
+
+           
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+
+
