03b76d1a4797faf68811929208d5381bcb8913cd
Crystal Low <crystallow93@gmail.com>
2016-12-07 00:30:59 -0500

    added boolean value to make button functionality disappear

--- MAIN.py
+++ MAIN.py
@@ -5,11 +5,34 @@ from instructions import instructionScreen
 from testingscreen import testScreen
 pygame.init()
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+red = (200, 0, 0)
+bright_red = (255, 0, 0)
+
+green = (0, 200, 0)
+bright_green = (0, 255, 0)
+
+blue = (0, 0, 200)
+bright_blue = (0, 0, 255)
+
+def instruction():
+    instruction = instructionScreen()
 def main():
+    screen = pygame.display.set_mode([800,600])
     
+    start = startScreen()
+    #startButton = button(green, bright_green, 350, 400, 150, 50)
+    #display_startscreen = True
+    #startscreen = 1
+   
+    mouse1 = pygame.mouse.get_pos()
+
+
     #instruction = instructionScreen()
     start = startScreen()
-    
+    var = True
     done = False
     while not done:
         
@@ -22,17 +45,27 @@ def main():
                mouse1 = pygame.mouse.get_pos() 
               
                #start.startButton      
-               if 500 > mouse1[0] > 350 and 450 > mouse1[1] > 400:
+               if 500 > mouse1[0] > 350 and 450 > mouse1[1] > 400 and var:
                    print("this start button works")                                                
+                   var = False 
                    instructionScreen()
                    
-               elif 475 > mouse1[0] > 325 and 550 > mouse1[1] > 500: 
+               elif 475 > mouse1[0] > 325 and 550 > mouse1[1] > 500 and not var: 
                   #print(mouse1)
                   print("this instructions button also works")
-                 
+                  var = False
                   testScreen()
-          
+                   
                   pygame.display.update()
             
-        pygame.display.update()
-main()
+        pygame.display.update()   
+    pygame.display.update()
+
+    
+            
+                
+main()                            
+                  
+
+            
+       
